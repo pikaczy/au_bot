3bf3a3fd0bb1a3b596951371c0621c432c5dadc2
jinzhengyang <jzysuzhou@gmail.com>
2021-02-09 21:28:25 +0800

    repair 2021/2/9 qqphone

--- app/qqPhone.py
+++ app/qqPhone.py
@@ -4,6 +4,6 @@ def qqPhone(content,redis_host):
     pool = redis.ConnectionPool(host=redis_host,port=6379,db=0,decode_responses=True)
     r = redis.Redis(connection_pool=pool)
     phone = r.sscan(content)
-    if(phone[0]==0):
+    if(len(phone[1])==0):
         return '无结果'
     return phone[1]
